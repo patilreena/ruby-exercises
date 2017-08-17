0758006dc387ff65be78e5451e8101cd08dd6d04
Reena Patil <reena.patil@outlook.com>
2017-08-17 22:19:10 +1000

    ruby case statement

---
+++ fruits.rb
@@ -0,0 +1,12 @@
+fruit = "apple"
+
+case fruit
+  when "pear"
+   puts "fruit is pear"
+  when "apple"
+   puts "fruit is apple"
+  when "banana"
+   puts "fruit is banana"
+ else
+   puts "not a fruit"
+end
